290628b5487e9a428d423a6dfe904e3504bc7fa3
katel0k <katelkin2003@mail.ru>
2025-01-16 00:33:42 +0300

    Made dynamic scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,33 @@ import { ReactNode, useState, useEffect } from "react";
 import { List, ListElement } from "./List/List";
 import { ListSettings, Settings } from "./Settings/Settings";
 import { constructAPIURL } from "./lib/utils";
+import "./App.module.css"
 
 export default function App(): ReactNode {
     const [ list, setList ] = useState<ListElement[]>([]);
     const [ settings, setSettings ] = useState<ListSettings>({
         sortOrder: 'asc'
     });
+    const [ page, setPage ] = useState<number>(1);
     useEffect(() => {
-        fetch(constructAPIURL(settings, 1))
-        .then(response => response.json())
-        .then(json => setList(json.items))
-    }, []);
+        (async () => {
+            let nextPageItems = await fetch(constructAPIURL(settings, page))
+                .then(response => response.json())
+                .then(json => json.items);
+            setList((prev: ListElement[]) => prev.concat(nextPageItems));
+            setIsLoading(false);
+        })();
+    }, [ page ]);
+    const [ isLoading, setIsLoading ] = useState<boolean>(false);
     return (
         <div className="wrapper">
             <Settings oldSettings={settings} handleChange={setSettings}/>
-            <List body={list} />
+            <List body={list} requestNewData={() => {
+                if (!isLoading) {
+                    setIsLoading(true);
+                    setPage(page + 1);
+                }
+            }} isLoading={isLoading} />
         </div>
     )
 }
--- src/List/List.tsx
+++ src/List/List.tsx
@@ -11,21 +11,29 @@ export type ListElement = {
     }
 }
 
+const REQUEST_FOR_NEW_DATA_THRESHOLD: number = 500;
+
 interface ListProps {
-    body: ListElement[]
+    body: ListElement[],
+    isLoading: boolean,
+    requestNewData: () => void
 }
 
-export function List({ body = [] }: ListProps): ReactNode {
+export function List({ body = [], isLoading, requestNewData }: ListProps): ReactNode {
     return (
-        <div styleName='body'>
-            {
-                body.map((a: ListElement, i: number) =>
-                    <div styleName='element' key={i}>
-                        <span>{a.name}</span>
-                        <a href={a.url}></a>
-                    </div>
-                )
-            }
+        <div styleName='body'
+            onScroll={e => {
+                if (e.currentTarget.getBoundingClientRect().bottom + 100 > REQUEST_FOR_NEW_DATA_THRESHOLD) {
+                    requestNewData();
+                }
+            }} >
+            {body.map((a: ListElement, i: number) =>
+                <div styleName='element' key={i}>
+                    <span>{a.name}</span>
+                    <a href={a.url}></a>
+                </div>
+            )}
+            {isLoading && (<div styleName='dataLoading'> Data is loading</div>)}
         </div>
     )
 }
